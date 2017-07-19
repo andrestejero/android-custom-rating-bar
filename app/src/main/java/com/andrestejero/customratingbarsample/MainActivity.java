7b6df5d0c5b3715e1961cd9e1edb1a0cf14c7239
Andres Tejero <atejero@garbarino.com.ar>
2017-07-19 12:32:51 -0300

    Add sample

--- app/src/main/java/com/andrestejero/customratingbarsample/MainActivity.java
+++ app/src/main/java/com/andrestejero/customratingbarsample/MainActivity.java
@@ -1,9 +1,10 @@
 package com.andrestejero.customratingbarsample;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
 import com.andrestejero.customratingbar.CustomRatingBar;
 
@@ -13,17 +14,15 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private int mRating = 0;
+    private CustomRatingBar mRatingBar1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example
-        TextView question = (TextView) findViewById(R.id.question);
-        question.setText("¿Qué tan fácil e intuitivo te resultó el uso de la APP?");
-
-        CustomRatingBar rating = (CustomRatingBar) findViewById(R.id.rating);
+        mRatingBar1 = (CustomRatingBar) findViewById(R.id.rating1);
 
         List<Integer> colors = new ArrayList<>();
         colors.add(R.color.yellow20);
@@ -31,12 +30,33 @@ public class MainActivity extends AppCompatActivity {
         colors.add(R.color.yellow60);
         colors.add(R.color.yellow80);
         colors.add(R.color.yellow100);
-        rating.setColorStarActive(colors);
+        mRatingBar1.setColorStarActive(colors);
 
-        rating.setOnStarChangeListener(new CustomRatingBar.OnStarChangeListener() {
+        mRatingBar1.setOnStarChangeListener(new CustomRatingBar.OnStarChangeListener() {
             @Override
             public void onStarChange(int stars) {
-                Log.d(LOG_TAG, "star: " + stars);
+                mRating = stars;
+            }
+        });
+
+        Button buttonContinue = (Button) findViewById(R.id.buttonContinue);
+        buttonContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mRating == 0) {
+                    mRatingBar1.showErrorStars();
+                    Snackbar.make(findViewById(R.id.content), getString(R.string.error), Snackbar.LENGTH_LONG).show();
+                } else {
+                    Snackbar.make(findViewById(R.id.content), getString(R.string.result, mRating), Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        CustomRatingBar ratingBar2 = (CustomRatingBar) findViewById(R.id.rating2);
+        ratingBar2.setOnStarChangeListener(new CustomRatingBar.OnStarChangeListener() {
+            @Override
+            public void onStarChange(int stars) {
+                Snackbar.make(findViewById(R.id.content), getString(R.string.result, stars), Snackbar.LENGTH_LONG).show();
             }
         });
     }
